63c4481c011301fc7bfa29aff2c9921ba5db6bde
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-02-11 21:52:20 +0100

    Add styling to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import CardGrid from './components/CardGrid';
+import ScoreDisplay from './components/ScoreDisplay';
 
 function App() {
   return (
     <div className='ultrawide-container'>
-      <CardGrid />
+      <div className='header'>
+        <div className='game-title'>
+          <h2 className='main-title'>Star Wars</h2>
+          <h3 className='sub-title'>A Memory Game</h3>
+        </div>
+        <ScoreDisplay />
+      </div>
+      <div className='main'>
+        <CardGrid />
+      </div>
     </div>
   );
 }
